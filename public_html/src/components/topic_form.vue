f66e2197b594f60686a67e660ef2c3dc8c4f049f
Wesley Ford <mangofordw@gmail.com>
2020-06-10 09:41:20 -0400

    new update

--- public_html/src/components/topic_form.vue
+++ public_html/src/components/topic_form.vue
@@ -1,28 +1,46 @@
 <template>
   <section class="section">
-    <h2 class="title">Sumbit your topic ideas here!</h2>
+    <h2 class="title">Submit your feedback or suggestions here</h2>
     <b-field label="Name">
-      <b-input v-model="name"></b-input>
+      <b-input :value="formName"></b-input>
     </b-field>
 
-    <b-field label="Email"
-      message="Please insert a valid email so we can email you back">
+    <b-field label="Email">
       <b-input type="email"
-        value="YourEmail@"
-        maxlength="30">
+        placeholder="youremail@example.com"
+        maxlength="30"
+        :value="formEmail">
       </b-input>
     </b-field>
 
-    <b-field label="Your Topic Idea" message="">
-      <b-input maxlength="200" type="textarea"></b-input>
+    <b-field label="Subject">
+      <b-input
+        maxlength="30" :value="formSubject">
+      </b-input>
     </b-field>
 
-    <b-button type="is-primary" size="is-medium">Submit!</b-button>
+    <b-field label="Body">
+      <b-input minlength="20" maxlength="200" type="textarea" :value="formBody"></b-input>
+    </b-field>
+
+    <b-button v-on:click="submitForm" type="is-primary" size="is-medium">Submit</b-button>
   </section>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      formName:'',
+      formEmail:'',
+      formSubject:'', 
+      formBody:''
+    }
+  },
+  methods: {
+    submitForm(){
+      console.log(this.formName, this.formEmail, this.formSubject, this.formBody);
+    }
+  },
 }
 </script>
