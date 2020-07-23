14c17b14926c85d1b6ae7c3a3d7d12235364b21c
Wesley Ford <mangofordw@gmail.com>
2020-07-22 23:40:23 -0400

    Fixed :value bug
    INstead of :value we now use the v-modal

--- public_html/src/components/topic_form.vue
+++ public_html/src/components/topic_form.vue
@@ -2,25 +2,25 @@
   <section class="section">
     <h2 class="title">Submit your feedback or suggestions here</h2>
     <b-field label="Name">
-      <b-input :value="formName"></b-input>
+      <b-input v-model="formName"></b-input>
     </b-field>
 
     <b-field label="Email">
       <b-input type="email"
         placeholder="youremail@example.com"
         maxlength="30"
-        :value="formEmail">
+        v-model="formEmail">
       </b-input>
     </b-field>
 
     <b-field label="Subject">
       <b-input
-        maxlength="30" :value="formSubject">
+        maxlength="30" v-model="formSubject">
       </b-input>
     </b-field>
 
     <b-field label="Body">
-      <b-input minlength="20" maxlength="200" type="textarea" :value="formBody"></b-input>
+      <b-input minlength="20" maxlength="200" type="textarea" v-model="formBody"></b-input>
     </b-field>
 
     <b-button v-on:click="submitForm" type="is-primary" size="is-medium">Submit</b-button>
